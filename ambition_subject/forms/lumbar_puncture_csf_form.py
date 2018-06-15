470e23f59b1d09545cd228800647c4c7be470f82
Coulson Thabo Kgathi <ckgathi@gmail.com>
2018-06-15 14:07:49 +0200

    allow 0 for quantitative_culture

--- ambition_subject/forms/lumbar_puncture_csf_form.py
+++ ambition_subject/forms/lumbar_puncture_csf_form.py
@@ -8,6 +8,14 @@ class LumbarPunctureCsfForm(SubjectModelFormMixin):
 
     form_validator_cls = LumbarPunctureCsfFormValidator
 
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        if cleaned_data.get('qc_requisition'):
+            if not cleaned_data.get("quantitative_culture") and not cleaned_data.get("quantitative_culture") == 0:
+                msg = 'Quantitative culture is required.'
+                self._errors["quantitative_culture"] = self.error_class([msg])
+        return cleaned_data
+
     class Meta:
         model = LumbarPunctureCsf
         fields = '__all__'
